5b49fc5033825a6f575402186d14f33a9eb29026
Anandhan <anandhanedom@gmail.com>
2020-11-03 12:31:41 +0530

    comments

--- src/Components/SignIn/SignIn.component.jsx
+++ src/Components/SignIn/SignIn.component.jsx
@@ -11,8 +11,8 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -97,10 +97,10 @@ class SignIn extends Component {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
+            {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
-            />
+            /> */}
             <Button
               type="submit"
               fullWidth
@@ -119,7 +119,7 @@ class SignIn extends Component {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2" style={{ color: '#000' }}>
+                <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
